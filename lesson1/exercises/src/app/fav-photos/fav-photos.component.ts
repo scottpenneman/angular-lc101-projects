8c9ef32cbf25048d9d1e5496aff01820f54d75c4
Scott <scott.penneman@gmail.com>
2019-12-10 21:01:10 -0600

    Completed Exercise 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i.etsystatic.com/5805234/r/il/7dc00d/1712348959/il_570xN.1712348959_dnfs.jpg';
+  image3 = 'https://www.mariowiki.com/images/5/54/Goomba_Movie.jpg';
 
   constructor() { }
 
